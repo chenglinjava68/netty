ba06ecbf7ce969336cf3850f7e8d90f8791369e2
chenglin <chenglin@hualala.com>
2016-08-26 13:16:51 +0800

    firset commit

---
+++ Netty-SpringMVC-master/src/main/java/org/springframework/sandbox/mvc/TestController.java
@@ -0,0 +1,19 @@
+package org.springframework.sandbox.mvc;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/hello")
+public class TestController {
+
+    @RequestMapping(value="/foo", produces = "text/html; charset=utf-8")
+    @ResponseBody 
+    public String getShopInJSON(HttpServletRequest request) {
+        return "{\"hello\":\"world\"}";
+    }
+
+}
